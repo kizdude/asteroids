b363a2d38c0d9b0dfda9f9c64d7fe9fbfbda15e4
kizdude <charlb0609@gmail.com>
2024-08-20 01:51:54 +0200

    asteroids and shots

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -9,14 +13,50 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    shots = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Shot.containers = (updatable, drawable, shots)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     # game loop
     while True:
-        screen.fill("black")
+        
         pygame.display.flip()
+        screen.fill("black")
+        
+        
+        for item in drawable:
+            item.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collisions(player):
+                print("Game over!")
+                return
+            for bullet in shots:
+                if asteroid.check_collisions(bullet):
+                    asteroid.kill()
+                    bullet.kill()
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ shot.py
@@ -0,0 +1,16 @@
+from circleshape import CircleShape
+import pygame
+from constants import *
+
+class Shot(CircleShape):
+    def __init__(self, x, y, velocity):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.position = pygame.Vector2(x, y)
+        self.radius = SHOT_RADIUS
+        self.velocity = velocity
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
